9dc51d142068a036f9bb18ecb7b7ad7828cef03c
almamarie <alouismariea97@gmail.com>
2023-01-09 23:44:47 +0000

    Committing

--- components/details-pane/CurrentItemDetails.tsx
+++ components/details-pane/CurrentItemDetails.tsx
@@ -1,10 +1,76 @@
-import { useAppSelector } from "../../store";
-import styles from "./CurrentItem.module.css";
+/* eslint-disable @next/next/no-img-element */
 
-const ItemDetails = () => {
+import { useAppSelector } from "../../store";
+import { DUMMY_ITEMS } from "../../store/items";
+import Button from "../ui/buttons/Button";
+import styles from "./ItemDetails.module.css";
+
+const CurrentItemDetails = () => {
   // fetch item id from store
-  const currentItemId = useAppSelector((state) => state.detailsPane.itemId);
-  return <h1>showing item {currentItemId}</h1>;
+  const itemId = useAppSelector((state) => {
+    return state.detailsPane.itemId;
+  });
+  // get id from database
+  const itemDetails = DUMMY_ITEMS.find((item) => {
+    return item.name.toLowerCase() === itemId;
+  });
+
+  console.log(itemDetails);
+
+  if (!itemDetails) {
+    return <p className={styles["item-not-found"]}>Item not found</p>;
+  }
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.back}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={styles["back-icon"]}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+          />
+        </svg>
+        <p>back</p>
+      </div>
+      <div className={styles["image-wrapper"]}>
+        <img
+          className={styles.image}
+          src={itemDetails.image}
+          alt={itemDetails.name}
+        />
+      </div>
+
+      <div className={styles.detail}>
+        <h3>{itemDetails.name}</h3>
+        <p>Avocado</p>
+      </div>
+
+      <div className={styles.detail}>
+        <h3>Category</h3>
+        <p>{itemDetails.category}</p>
+      </div>
+
+      <div className={styles.detail}>
+        <h3>Note</h3>
+        <p>{itemDetails.notes}</p>
+      </div>
+      <div className={styles.buttons}>
+        <Button category="delete" type="button">
+          delete
+        </Button>
+        <Button category="submit" type="button">
+          Add to list
+        </Button>
+      </div>
+    </div>
+  );
 };
 
-export default ItemDetails;
+export default CurrentItemDetails;
