f18c79764ab168be3b4d6c13af0869956c3059f1
almamarie <alouismariea97@gmail.com>
2023-01-09 23:47:35 +0000

    Committing

---
+++ store/category-list.ts
@@ -0,0 +1,80 @@
+export const DUMMY_LIST =
+  // TODO: Add pictures and better context for each
+  {
+    "Fruits and vegetables": [
+      {
+        name: "Avocado",
+      },
+      {
+        name: "Banana",
+      },
+      {
+        name: "Bunch of sarrots 5pcs",
+      },
+      {
+        name: "Chicken 1kg",
+      },
+      {
+        name: "Pre cooked corn 450g",
+      },
+
+      {
+        name: "Mandarin Nadorcott",
+      },
+      {
+        name: "Piele De Sapo Melon",
+      },
+      {
+        name: "Watermelon",
+      },
+    ],
+
+    //   TODO: Add pictures of each
+    "Meat and Fish": [
+      {
+        name: "Chicken leg box",
+      },
+
+      {
+        name: "Chicken 1kg",
+      },
+      {
+        name: "Pork fillets 450g",
+      },
+      {
+        name: "Salmon 1kg",
+      },
+    ],
+
+    //TODO: Add real Beverages and their pistures
+
+    Beverages: [
+      {
+        name: "coca-cola",
+      },
+
+      {
+        name: "malta",
+      },
+      {
+        name: "sprite",
+      },
+      {
+        name: "pepsi",
+      },
+
+      {
+        name: "beer",
+      },
+    ],
+
+    // TODO: Add more pets and their pictures
+
+    Pets: [
+      {
+        name: "Salmon 1kg",
+      },
+    ],
+  };
+
+export const DUMMY_CATEGORIES = Object.keys(DUMMY_LIST);
