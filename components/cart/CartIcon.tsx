36f2606fd26054bd1b8c817ee9f878c03e5d6201
almamarie <alouismariea97@gmail.com>
2023-01-12 12:36:25 +0000

    integrated totalItems with the cart icon in the vaviagtion pane

--- components/cart/CartIcon.tsx
+++ components/cart/CartIcon.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import logo from "../../public/svgs/shopping-cart.svg";
+import { useAppSelector } from "../../store";
 import styles from "./CartIcon.module.css";
 const CartIcon = () => {
+  const totalItems = useAppSelector((state) => state.cart.totalQuantity);
   return (
     <div className={styles.wrapper}>
       <Image className={styles.logo} src={logo} alt="shopping cart logo" />
 
-      <p>3</p>
+      <p>{totalItems}</p>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>All Items</title>
+        <title>Shopingify - All Items</title>
         <meta name="description" content="All items in the shopping list" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
